00bb71ebd0b478d375db234b32ac55fb58b5d797
siddharth-mitra <54763534+siddharth-mitra@users.noreply.github.com>
2019-12-07 17:25:09 +0100

    Add files via upload

--- Assignment3/IS-GIS-Lab/IS-GIS-Lab/code/quadtree.py
+++ Assignment3/IS-GIS-Lab/IS-GIS-Lab/code/quadtree.py
@@ -1,121 +1,111 @@
 import boundingbox as bb
 import math
 
+
 class QuadTree:
-	"""
-	The QuadTree class is a tree data structure in which each internal
-	non leave node has exactly four children.
-	
-	the internal data structre `self.quads = {}` is a dictionary where its
-	keys represent depth (starting from 0) and has a list of BoundingBoxes representing
-	the partions/space. The total area of at each level should remain equal to the initial
-	bounding box.
-	
-	self.quads = 
-	{
-		0: [1    x BoundingBox ]
-		1: [4    x BoundingBox ]
-		.. 
-		n: [4**n x BoundingBox ]
-	}
-	
-	Generation of the QuadTree is slow, using it to reduce your dataset it fast.
+    """
+    The QuadTree class is a tree data structure in which each internal
+    non leave node has exactly four children.
 
-	"""
-	def __init__(self,bbox, depth):
-		"""
-		Create a new QuadTree instance.
-		:param bbox: the initial BoundingBox
-		:param depth: the depth of the QuadTree
+    the internal data structre `self.quads = {}` is a dictionary where its
+    keys represent depth (starting from 0) and has a list of BoundingBoxes representing
+    the partions/space. The total area of at each level should remain equal to the initial
+    bounding box.
 
-		:Example:
-		>>> bbox = bb.BoundingBox(2,9,1,7)
-		>>> qt = QuadTree(bbox, 2)
-		"""
-		self.quads = {}
-		self.depth = depth
+    self.quads =
+    {
+        0: [1    x BoundingBox ]
+        1: [4    x BoundingBox ]
+        ..
+        n: [4**n x BoundingBox ]
+    }
 
-		for x in range(depth):
-			self.quads[x]= []
+    Generation of the QuadTree is slow, using it to reduce your dataset it fast.
 
-		self.quads[0] = [bbox]
-		self.recurse(bbox, 1)
+    """
 
-	def recurse(self,bbox, depth):
-		"""
-		Internal function called on class contruction, this should 
-		create the BoundingBoxes.
+    def __init__(self, bbox, depth):
+        self.quads = {}
+        self.depth = depth
+        for x in range(depth):
+            self.quads[x] = []
+        self.quads[0] = [bbox]
+        self.recurse(bbox, 1)
 
-		:param bbox: the initial BoundingBox
-		:param depth: the depth of the QuadTree
+    def recurse(self, _bbox, depth):
 
-		:To be implemented by the student:		
-		"""
-		raise NotImplementedError(":To be implemented by the student:")
+        if depth >= self.depth:
+            return
+        print(depth)
+        height = _bbox.height()
+        width = _bbox.width()
+        minx = _bbox.data[0, 0]
+        maxx = _bbox.data[0, 1]
+        miny = _bbox.data[1, 0]
+        maxy = _bbox.data[1, 1]
+        nw = bb.BoundingBox(minx, minx + (width / 2), miny, miny + (height / 2))
+        ne = bb.BoundingBox(minx + (width / 2), maxx, miny, miny + (height / 2))
+        sw = bb.BoundingBox(minx, minx + (width / 2), miny + (height / 2), maxy)
+        se = bb.BoundingBox(minx + (width / 2), maxx, miny + (height / 2), maxy)
+        self.quads[depth] += [nw]
+        self.quads[depth] += [ne]
+        self.quads[depth] += [sw]
+        self.quads[depth] += [se]
 
-	@staticmethod	
-	def at_least(size):
-		"""
-		Returns the amount of BoundingBoxes when the user
-		request `at least` an amount of bboxes. The returned
-		value is >= than size.
-		
-		:param size: minimum requested size
+        depth = depth + 1
+        self.recurse(nw, depth)
+        self.recurse(ne, depth)
+        self.recurse(sw, depth)
+        self.recurse(se, depth)
 
-		:Example:
-		>>> print(QuadTree.at_least(900))
-		>>> 1024	
-		"""
-		return 4**int(math.ceil(math.log(size,4)))
+    @staticmethod
+    def at_least(size):
 
-	@staticmethod	
-	def at_most(size):
-		"""
-		Returns the amount of BoundingBoxes when the user
-		request `at most` an amount of bboxes. The returned
-		value is <= than size.
-		
-		:param size: maximum requested size
+        """
+        Returns the amount of BoundingBoxes when the user
+        request `at least` an amount of bboxes. The returned
+        value is >= than size.
 
-		:Example:
-		>>> print(QuadTree.at_most(900))
-		>>> 256
-		"""
-		return 4**int(math.floor(math.log(size,4)))
+        :param size: minimum requested size
 
-	@staticmethod	
-	def level(size):
-		"""
-		Returns the level required (rounded up)
-		for a given size of elements. 
-		return int(math.ceil(math.log(size,4)))
-	
-		:param size: requested size
-		>>> print(QuadTree.level(1))
-		>>> 0
-		>>> print(QuadTree.level(5))
-		>>> 2
-		"""
-		return int(math.ceil(math.log(size,4)))
+        :Example:
+#		>>> print(QuadTree.at_least(900))
+#		>>> 1024
+        """
+        return 4 ** int(math.ceil(math.log(size, 4)))
 
-	def quadrants(self):
-		"""
-		Returns the quads member
-		"""
-		return self.quads
+
+@staticmethod
+def at_most(size):
+    """
+    Returns the amount of BoundingBoxes when the user
+    request `at most` an amount of bboxes. The returned
+    value is <= than size.
+
+    :param size: maximum requested size
+
+    :Example:
+#		>>> print(QuadTree.at_most(900))
+#		>>> 256
+    """
+    return 4 ** int(math.floor(math.log(size, 4)))
+
+
+@staticmethod
+def level(size):
+    return int(math.ceil(math.log(size, 4)))
+
+
+def quadrants(self):
+    return self.quads
+
 
 if __name__ == '__main__':
 
-	bbox = bb.BoundingBox(2,9,1,7)
-	print(QuadTree.at_least(900))
-	print(QuadTree.at_most(900))
-	print(QuadTree.level(1))
-	print(QuadTree.level(5))
-	print(QuadTree.level(900))
+    bbox = bb.BoundingBox(0, 10, 0, 10)
 
-	qt = QuadTree(bbox, 2)
-	for k,v in qt.quads.items():
-		print (k,len(v))
-		for x in v:
-			print(x.data)
-
+    qt = QuadTree(bbox, 3)
+    for k, v in qt.quads.items():
+        print(k, len(v))
+        for x in v:
+            print(x.data)
