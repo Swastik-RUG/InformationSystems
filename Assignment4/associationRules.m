0619780bb9b9270f29ae9379e5e9d199ffa3cd44
Siddhart Mitra <siddharth.snu@gmail.com>
2019-12-10 19:43:27 +0100

    Assignment 4

---
+++ Assignment4/associationRules.m
@@ -0,0 +1,55 @@
+% Course: Information Systems
+% Association Rule Analysis with Apriori
+% Author: Dr. George Azzopardi
+% Date: December 2019
+
+% input parameters: minsup = minimum support, minconf = minimum confidence
+function associationRules(minsup,minconf)
+shoppingList = readDataFile;
+
+ntrans = size(shoppingList,1);
+items = unique([shoppingList{:}]);
+nitems = numel(items);
+
+[tridx,trlbl] = grp2idx(items);
+
+% Create the binary matrix
+dataset = zeros(ntrans,nitems);
+for i = 1:ntrans
+   dataset(i,tridx(ismember(items,shoppingList{i}))) = 1;
+end
+
+% Generate frequent items of length 1
+support{1} = sum(dataset)/ntrans;
+f = find(support{1} >= minsup);
+frequentItems{1} = tridx(f);
+support{1} = support{1}(f);
+
+% Generate frequent item sets
+k = 1;
+while k < nitems && size(frequentItems{k},1) > 1
+    % Generate length (k+1) candidate itemsets from length k frequent itemsets
+    frequentItems{k+1} = [];
+    support{k+1} = [];
+    
+    % Consider joining possible pairs of item sets
+    for i = 1:size(frequentItems{k},1)-1
+        for j = i+1:size(frequentItems{k},1)
+            if k == 1 || isequal(frequentItems{k}(i,1:end-1),frequentItems{k}(j,1:end-1))
+                candidateFrequentItem = union(frequentItems{k}(i,:),frequentItems{k}(j,:));  
+                if all(ismember(nchoosek(candidateFrequentItem,k),frequentItems{k},'rows'))                
+                    sup = sum(all(dataset(:,candidateFrequentItem),2))/ntrans;                    
+                    if sup >= minsup
+                        frequentItems{k+1}(end+1,:) = candidateFrequentItem;
+                        support{k+1}(end+1) = sup;
+                    end
+                end
+            else
+                break;
+            end            
+        end
+    end         
+    k = k + 1;
+end
+
+% Generate association rules. To be implemented by students
