88e1fb1efe3d45b04ad697760afb9dd815fb3075
Swastik <swastiK23>
2019-12-17 04:21:44 +0100

    Adding Performance fixes

--- Assignment4/associationRulesAntinMonotone.m
+++ Assignment4/associationRulesAntinMonotone.m
@@ -56,29 +56,49 @@ generateAssociation(frequentItems,support, minconf)
 end
 
 % Generate association rules. To be implemented by students
-
 function generateAssociation(frequentItems, support, minconf)
 tic
-confidenceList = {};
+% Data-Structure to store the association rules that satisfy min-confidence
+associationRules = {};
+% Iterate over the Frequent Items level in reverse order
 for i=size(frequentItems,2)-1:-1:2
-    tmpfi = frequentItems(i);
-    tmpfi = tmpfi{1};
+    % Iterate over the Frequent items
     for k =1:size(frequentItems{1,i},1)
+        % Store the Frequent Item at level 'i' and Position 'k'
         row = frequentItems{1,i}(k,:);
+        % Get the support value for the antecedent (sup(AUB) or numerator)
         support_nume = support{1, size(row, 2)}(k);
+        % Create a list to store the subsets with confidence less than min-conf
+        % The subsets will be pruned and it is discontinued susequent stages
         pruneList = [];
+        % Iterate over row to generate association rules
         for j=length(row)-1:-1:1
+            % Create subsets of row with size 'j'
             subsets = nchoosek(row,j);
+            % Prune the subsets from the list of subsets generated with
+            % confidence less than min-confidence
             if  isempty(pruneList) isSubset = []; else isSubset = ismember(subsets, pruneList{1}); end
             subsets(all(isSubset==1,2),:) = [];
+            % Iteration over the subsets that are not pruned
             for x =1:size(subsets,1)
+                % Get the antecedent
                 X_S = subsets(x,:);
+                % Get the Frequent Item set that belongs to antecedent
+                % Ex: if antecdent = [1, 2]; FrequentItem(2) will be used
                 fi_X_S = frequentItems{size(X_S,2)};
+                % Find the index of the antecedent in the FrequentItem
+                % Use the Index found to fetch the support of antecedent
                 support_X_S = support{size(X_S,2)}(ismember(fi_X_S, X_S, 'rows'));
+                %Calculate confidence value
                 confidence = round(support_nume/support_X_S,2);
+                % If confidence is greater than minimum confidence store it
+                % in the associationRule Data-structure as results
                 if confidence >= minconf
-                    confidenceList = [confidenceList;[{X_S}, {size(X_S,2)} {setdiff(row,X_S)}, {confidence}]];
+                    associationRules = [associationRules;[{X_S}, {setdiff(row,X_S)}, {confidence}]];
                 else
+                    % If the confidence is less than the min-confidence
+                    % add an entry in the prune list to skip the
+                    % computation of its subset association rules.
                     pruneList{end+1,1} = X_S;
                 end
             end
@@ -86,26 +106,33 @@ for i=size(frequentItems,2)-1:-1:2
     end
 end
 toc
-show_top_30(confidenceList);
+% Display the top 30 association rules
+show_top_30(associationRules);
 end
 
-function show_top_30(confidenceList)
+function show_top_30(associationRules)
+    % Open the dataset file
     fid = fopen('myDataFile.csv');
-    a = textscan(fid,'%s',1);
+    % Get the header of the CSV file that contains the item names
+    items = textscan(fid,'%s',1);
+    % close the file
     fclose(fid);
-    header = split(a{1},',');
-    sortedRes = sortrows(confidenceList,4,'descend');
-    toDisp =sortedRes(1:30,:);
-    for i=1:size(toDisp,1)
-        c1 = toDisp(i,1);
-        c1 = arrayfun(@(x) header(x),c1{1});
-        c2 = toDisp(i,2);
-        c2 = arrayfun(@(x) header(x),c2{1});
-        c3 = toDisp(i,4);
-        a = strjoin(c1);
-        b = strjoin(c2);
-        ax = c3{1};
-        fprintf("%s => %s Confidence = %f \n", a, b, ""+ax)
+    % Split the header by delimiter comma to obatain the list of item names
+    header = split(items{1},',');
+    % Sort the association rules in descending order
+    sortedRes = sortrows(associationRules,3,'descend');
+    % pick the top 30 association rules from the sorted list
+     sortedRes = sortedRes(1:30,:);
+    % For each of the picked association rules
+    for i=1:size(sortedRes,1)
+        % retrive the original name of the item from the header extracted 
+        % from the dataset file and concatenate it to a string
+        antecedent = sprintf(' %s,' , header{sortedRes{i,1}});
+        consequent = sprintf(' %s,' , header{sortedRes{i,2}});
+        % Get the confidence value for the antecedent => consequent
+        confidence = sprintf(' %f' , sortedRes{i,3});
+        % Print the association Rule
+        fprintf("{%s} => {%s} ::: Confidence = %s \n", antecedent(1:end-1), consequent(1:end-1), ""+confidence)
     end
 end
 
--- Assignment4/plotHistogram.m
+++ Assignment4/plotHistogram.m
@@ -1,6 +1,6 @@
 function r = plotHistogram(data)
     figure('units','normalized','outerposition',[0 0 1 1],'NumberTitle', 'off', 'Name', "PRODUCTS FREQUENCY")
-    h = histogram(categorical([data{:}]'),'Orientation', 'vertical','facecolor', [1 0.301960784313725 0])
+    h = histogram(categorical([data{:}]'),'Orientation', 'vertical','facecolor', [1 0.301960784313725 0]);
     xtickangle(90)
     xlabel(['\fontsize{16}PRODUCTS'])
     ylabel(['\fontsize{16}FREQUENCY'])
